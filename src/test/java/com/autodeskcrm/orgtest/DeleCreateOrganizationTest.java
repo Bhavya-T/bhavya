ccf96076b69196513ceb3a2c50f1024dbd4c1828
bhavya <bhavya27t@gmail.com>
2020-08-04 15:06:33 +0530

    cummit and push

---
+++ src/test/java/com/autodeskcrm/orgtest/DeleCreateOrganizationTest.java
@@ -0,0 +1,93 @@
+package com.autodeskcrm.orgtest;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.autodeskcrm.gerericutils.BaseClass;
+import com.autodeskcrm.gerericutils.ExcelLib;
+import com.autodeskcrm.gerericutils.FileLib;
+import com.autodeskcrm.gerericutils.WebDriverUtils;
+/**
+ * 
+ * @author Bhavya
+ *
+ */
+public class DeleCreateOrganizationTest extends BaseClass {
+	
+	@Test
+	public void createORgtest() throws Throwable {
+		
+		
+		
+
+		
+		/* read test script specific data*/
+		String orgName = excelLib.getExcelData("org", 1, 2)+ "_"+ wLib.getRamDomNum();
+		String org_Type = excelLib.getExcelData("org", 1, 3);
+		String org_industry = excelLib.getExcelData("org", 1, 4);
+		
+		
+		 
+		 
+		
+		
+		/*step 3 : navigate to Org page*/
+		driver.findElement(By.linkText("Organizations")).click();
+		
+		
+		/*step 4 : navigate to create new Org page*/
+		driver.findElement(By.xpath("//img[@alt='Create Organization...']")).click();
+		
+		/*step 5 : create Org*/
+		driver.findElement(By.name("accountname")).sendKeys(orgName);
+	
+		
+		WebElement  swb1 = driver.findElement(By.name("accounttype"));
+	    wLib.select(swb1, org_Type);
+				
+		WebElement  swb2 = driver.findElement(By.name("industry"));
+		wLib.select(swb2, org_industry);
+				
+	   driver.findElement(By.xpath("//input[@title='Save [Alt+S]']")).click();
+	
+	
+	
+
+		/*step 6 : verify the Org*/
+		String actOrgName = driver.findElement(By.xpath("//span[@class='dvHeaderText']")).getText();
+
+		Assert.assertTrue(actOrgName.contains(orgName));
+		
+		/*step 7 :navigate to org page*/
+		driver.findElement(By.linkText("Organizations")).click();
+		
+		
+	    /* step 8 :select org to del*/
+		driver.findElement(By.id("145")).click();
+		
+		/* step 9 : click on del*/
+		driver.findElement(By.xpath("//input[@class='crmbutton small delete']")).click();
+		
+		/*step : 10 alert popup*/
+		
+		wLib.alertOk(driver);
+		
+		/*send org name to serch text field*/
+		driver.findElement(By.xpath("//input[@class='txtBox']")).sendKeys(orgName);
+		
+		/* step11 : click on search now  */
+		driver.findElement(By.xpath("//input[@name=\"submit\"]")).click();
+		
+		
+		
+		
+	}
+
+}
